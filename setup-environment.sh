14dfa6790b535141892faa78d556cec2a1a4989a
FelipeSSDev <13539403+FelipeSSDev@users.noreply.github.com>
2023-01-14 11:18:04 -0300

    fix: remove sudo from oh-my-zsh installation

--- setup-environment.sh
+++ setup-environment.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/sh
 
 ### Oh My ZSH
-sudo sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 git clone https://github.com/lukechilds/zsh-nvm ~/.oh-my-zsh/custom/plugins/zsh-nvm 
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
