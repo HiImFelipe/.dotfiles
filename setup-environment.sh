a5c5942fa9ff9504086615634b973f6dc01cb02a
FelipeSSDev <13539403+FelipeSSDev@users.noreply.github.com>
2023-02-08 20:32:13 -0300

    feat: put theme and fixes

--- setup-environment.sh
+++ setup-environment.sh
@@ -25,13 +25,23 @@ unzip /tmp/JetBrainsMono.zip -d ~/.fonts/JetBrainsMono
 ### Setup GNOME
 mkdir -p ~/.themes
 
+git clone https://github.com/vinceliuice/Orchis-theme.git
+cd Orchis-theme
+./install.sh
+./install.sh -l
+cd ~
+
 wget -O /tmp/adw-gtk3v4-2.tar.xz https://github.com/lassekongo83/adw-gtk3/releases/download/v4.2/adw-gtk3v4-2.tar.xz &&
 tar -xvf /tmp/adw-gtk3v4-2.tar.xz -C ~/.themes
 
 git clone https://github.com/vinceliuice/Tela-circle-icon-theme /tmp/Tela-circle-icon-theme
 bash -C /tmp/Tela-circle-icon-theme/install.sh -a
 
-gsettings set org.gnome.desktop.wm.preferences button-layout "close,minimize,maximize:"
+gsettings set org.gnome.desktop.wm.preferences button-layout ":minimize,maximize,close"
+
+## Theme fixes
+paru -S stylepak-git
+flatpak override --user --filesystem=xdg-config/gtk-4.0
 
 ### Setup VSCode
 paru -S visual-studio-code-bin
